4f52dfc982697a9a2cea63b1343aa5bacff0312d
Bharathigm <68587893+Bharathigm@users.noreply.github.com>
2021-02-26 22:04:35 +0530

    tested

---
+++ JProgram/src/Basics/Circle.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Circle 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int r=5;
+        double pi=3.14;
+        double cir=pi*r*r;
+        System.out.println(cir);
+    }
+}
---
+++ JProgram/src/Basics/Rectangle.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Rectangle 
+{
+    public static void main(String[] args) throws Exception 
+    {
+    int a=2;
+    int b=3;
+    int c=a*b;
+    System.out.println(c);
+    }
+}
