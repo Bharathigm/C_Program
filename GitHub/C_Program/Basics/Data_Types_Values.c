d005330468f7f4585a4efff26cadf4aed22facd9
Bharathigm <68587893+Bharathigm@users.noreply.github.com>
2021-03-24 13:20:46 +0530

    Tested

---
+++ GitHub/C_Program/Basics/Data_Types_Values.c
@@ -0,0 +1,21 @@
+/* Program to display minimum and maximum values of different data types */
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    printf("CHAR_MIN  =%d \n",CHAR_MIN);
+    printf("CHAR_MAX  = %d \n",CHAR_MAX);
+    printf("SCHAR_MIN =%d \n",SCHAR_MIN);
+    printf("SCHAR_MAX = %d \n",SCHAR_MAX);
+
+    printf("INT_MIN   =%d \n",INT_MIN);
+    printf("INT_MAX   = %d \n",INT_MAX);
+    printf("UINT_MAX  = %u \n",UINT_MAX);
+    printf("SHRT_MIN  =%d \n",SHRT_MIN);
+    printf("SHRT_MAX  = %d \n",SHRT_MAX);
+
+    printf("LONG_MIN  =%ld \n",LONG_MIN);
+    printf("LONG_MAX  = %ld \n",LONG_MAX);
+    printf("ULONG_MAX = %lu \n",ULONG_MAX );
+
+}
