cea11199359688e29bbc3c414c480e327bcd1c92
Bharathigm <68587893+Bharathigm@users.noreply.github.com>
2021-04-09 20:35:27 +0530

    Tested

---
+++ GitHub/C_Program/Basics/100_door_puzzle_Complexity_n.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main()
+{
+        int a[100],i,j,n=100;
+        printf("initially all doors are closed");
+        for(i=1;i<=n;i++)
+        {
+            a[i]=0;
+        }
+        
+        for(i=1;i<=n;i++)
+        {
+            a[i]=1;
+        }
+       /* for(i=1;i<=n;i++)
+        {
+            printf("Iternation 1=");      
+            printf("\n%d=%d ",i, a[i]);
+        }*/
+     for(i=1;i<=n;i++)
+       {
+         for(j=i;j<=n;)
+         {
+             if(i==1)
+             {
+                 printf("\n");
+                 printf("Iternation %d=",i);
+                    if(a[j]==1)
+                    {
+                      a[j]=0;
+                      printf("%d=%d ",j, a[j]);
+                    }
+                    else
+                    {
+                     a[j]=1;
+                     printf("%d=%d ",j, a[j]);
+                    }
+                        j=j+1;
+              }
+            printf("\n");
+            printf("Iternation %d=",i);
+             if(a[j]==1)
+             {
+                 a[j]=0;
+                  printf("%d=%d ",j, a[j]);
+             }
+             else
+             {
+                 a[j]=1;
+                 printf("%d=%d ",j, a[j]);
+             }
+                     j=i*j;
+             }
+             
+           
+         }
+         printf("\n");
+          for(i=1;i<=n;i++)
+        {
+            printf("\n%d=%d ",i, a[i]);
+        }
+         
+}
---
+++ GitHub/C_Program/Basics/Organic farming.py
@@ -0,0 +1,67 @@
+farmer
+{
+    "farmLicenseNumber":"OFarm0001",
+    "farmerName":"Ganesh",
+    "farmerGeoLocation":"12.2958","76.6395",
+    "farmSize":"15 Acres",
+    "soilHealthCardNumber":"SHC00036789",
+    "soilType":"red loamy soil",
+    "avgTemperature":"28 degree celcius",
+    "irrigatedRainfed":"Rainfed"
+    "productSownDate":"01/01/2021",
+    "typeOfProduct":"millet",
+    "OrganicManurePercentUsed":"75%",
+    "OrganicPesticidePErcentUSed":"5%",
+    "harvestDate":"05/03/2021",
+    "readyToSellDate":"20/03/2021"
+}
+
+Packer
+{
+    "packerLicenseNumber":"PLN001",
+    "packerName":"Maya",
+    "packerGeoLocation":"12.2965","76.6300 ",
+    "quantity":"1 ton",
+    "packageDate":"22/03/2021",
+    "packageCondition":"Fine",
+    "packageweight":"1 kilogram",
+}
+
+Processor
+{
+    "productiondate":"23/03/2021",
+    "processingTime":"16:30",
+    "packingInformation":"packed",
+    "storageConditio":"cool/dry"
+    "labTestingResults":"Fine",
+}
+
+Shipper
+{
+    "shipperLicenseNumber":"SLN001",
+    "shipperName":"Anitha",
+    "shippingDate":"24/03/2021",
+    "deliveryDate":"25/03/2021",
+    "deliveredTo":"OraganicShop",
+    "TransportCondition":"cool/dry",
+    "gpsInfoofFrom":"12.2965","76.6300",
+    "gpsinfoofTo":"12.9716","77.5946",
+}
+
+Retailer
+{
+    "retailerLicenseNumber":"RLN001",
+    "retailerName":"Bharathi",
+    "retailerGeoLocation":"12.9716","77.5946",
+    "retailerreceivingDate":"25/03/2021",
+    "productExpirationDate":"25/03/ ",
+    "storageCondition":"cool/dry",
+    "RetailerInventoryInfo":"100 kilogram"
+}
+
+Consumer
+{
+    "visualInformation":"yes",,
+    "consumerMobileFriendlyApp":" Yes",
+    "consumerFeedback":"Good"
+}
